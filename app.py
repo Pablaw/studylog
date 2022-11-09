624627d501e6116bdfc73912ffeefd56fedcee97
pablaw <chulhwa7@gmail.com>
2022-11-09 11:28:16 +0900

    feat: 프로필링크 연결

--- app.py
+++ app.py
@@ -21,23 +21,25 @@ def getContributions():
 
    log_all = soup.select_one('#user-profile-frame > div > div.mt-4.position-relative > div.js-yearly-contributions > div > div > div > svg > g > g:nth-child(53) > rect:nth-last-child(1)')
 
-   print(log_all)
+   month3 = soup.select_one('#user-profile-frame > div > div.mt-4.position-relative > div.js-yearly-contributions > div > div > div > svg > g > text:nth-child(62)')
+   month2 = soup.select_one('#user-profile-frame > div > div.mt-4.position-relative > div.js-yearly-contributions > div > div > div > svg > g > text:nth-child(63)')
+   month1 = soup.select_one('#user-profile-frame > div > div.mt-4.position-relative > div.js-yearly-contributions > div > div > div > svg > g > text:nth-child(64)')
+   month0 = soup.select_one('#user-profile-frame > div > div.mt-4.position-relative > div.js-yearly-contributions > div > div > div > svg > g > text:nth-child(65)')
+
+
+
    today = log_all['data-date']
    work = log_all['data-level']
+   d_3month =  month3.text
+   d_2month = month2.text
+   d_1month = month1.text
+   d_0month = month0.text
 
-   return jsonify({'response': [today, work]})
+   print(log_all, d_0month, d_1month, d_2month, d_3month)
+   return jsonify({'gitToday': [today, work], 'gitGrass': [d_0month, d_1month, d_2month, d_3month]})
 
 
 
 if __name__ == '__main__':
    app.run('0.0.0.0',port=7778,debug=True)
 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('https://github.com/Pablaw', headers=headers)
-
-soup = BeautifulSoup(data.text, 'html.parser')
-
-log_all = soup.select_one('body > div.application-main > main > div.container-xl.px-3.px-md-4.px-lg-5 > div > div.Layout-main > div:nth-child(2) > div > div.mt-4.position-relative > div.js-yearly-contributions > div > div > div > svg > g > g:nth-child(53) > rect:nth-last-child(1)')
-print(log_all)
-today = log_all['data-date']
-work = log_all['data-level']
--- test.py
+++ test.py
@@ -6,15 +6,24 @@ from bs4 import BeautifulSoup
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 data = requests.get('https://github.com/Pablaw', headers=headers)
+
 soup = BeautifulSoup(data.text, 'html.parser')
 
-log1 = soup.select('body > div.application-main > main > div.container-xl.px-3.px-md-4.px-lg-5 > div > div.Layout-main > div:nth-child(2) > div > div.mt-4.position-relative > div.js-yearly-contributions > div > div > div > svg > g > g:nth-child(53)')
-log2 = soup.select('body > div.application-main > main > div.container-xl.px-3.px-md-4.px-lg-5 > div > div.Layout-main > div:nth-child(2) > div > div.mt-4.position-relative > div.js-yearly-contributions > div > div > div > svg > g > g:nth-child(51)')
+log_all = soup.select_one('body > div.application-main > main > div.container-xl.px-3.px-md-4.px-lg-5 > div > div.Layout-main > div:nth-child(2) > div > div.mt-4.position-relative > div.js-yearly-contributions > div > div > div > svg > g > g:nth-child(53) > rect:nth-last-child(1)')
 
-log_all = soup.select_one('#user-profile-frame > div > div.mt-4.position-relative > div.js-yearly-contributions > div > div > div > svg > g > g:nth-child(53) > rect:nth-last-child(1)')
+month1 = soup.select_one('#user-profile-frame > div > div.mt-4.position-relative > div.js-yearly-contributions > div > div > div > svg > g > text:nth-child(62)')
+month2 = soup.select('#user-profile-frame > div > div.mt-4.position-relative > div.js-yearly-contributions > div > div > div > svg > g > text:nth-child(63)')
+month3 = soup.select('#user-profile-frame > div > div.mt-4.position-relative > div.js-yearly-contributions > div > div > div > svg > g > text:nth-child(64)')
+month4 = soup.select('#user-profile-frame > div > div.mt-4.position-relative > div.js-yearly-contributions > div > div > div > svg > g > text:nth-child(65)')
+print(month1, month2, month3, month4)
+
 # today = log_all['data-date']
 # work = log_all['data-level']
 
-print(log_all);
+#user-profile-frame > div > div.mt-4.position-relative > div.js-yearly-contributions > div > div > div > svg > g > text:nth-child(62)
+#user-profile-frame > div > div.mt-4.position-relative > div.js-yearly-contributions > div > div > div > svg > g > text:nth-child(65)
+#user-profile-frame > div > div.mt-4.position-relative > div.js-yearly-contributions > div > div > div > svg > g > text:nth-child(65)
 
-# 1, 2~3, 4~5, 6
+#user-profile-frame > div > div.mt-4.position-relative > div.js-yearly-contributions > div > div > div > svg > g > text:nth-child(54)
+#user-profile-frame > div > div.mt-4.position-relative > div.js-yearly-contributions > div > div > div > svg > g > text:nth-child(65)
+#user-profile-frame > div > div.mt-4.position-relative > div.js-yearly-contributions > div > div > div > svg > g > text:nth-child(64)
